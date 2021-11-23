5a60814db6fded52880154c0842cd1439acd3095
VadiuninDA <VadiuninDA@lad24.ru>
2021-11-23 15:44:02 +0300

    debug: добавил карты

--- src/screens/tabs/tabMapScreen/index.tsx
+++ src/screens/tabs/tabMapScreen/index.tsx
@@ -1,11 +1,29 @@
 import * as React from 'react';
 
-import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import {SafeAreaView, StyleSheet, View, Text} from 'react-native';
+import MapView, {Marker} from 'react-native-maps';
 
 const TabMapScreen = () => {
+  const initialRegion = {
+    latitude: 37.72825,
+    longitude: -122.4324,
+    latitudeDelta: 0.25,
+    longitudeDelta: 0.15,
+  };
   return (
     <SafeAreaView style={styles.container}>
       <Text>Tab Map Screen</Text>
+      <View>
+        <MapView style={styles.map} initialRegion={initialRegion}>
+          <Marker coordinate={{latitude: 37.78825, longitude: -122.4324}} />
+          <Marker coordinate={{latitude: 37.78525, longitude: -122.4224}} />
+          <Marker coordinate={{latitude: 37.7765, longitude: -122.4124}} />
+          <Marker coordinate={{latitude: 37.7965, longitude: -122.4424}} />
+          <Marker coordinate={{latitude: 37.8065, longitude: -122.4424}} />
+          <Marker coordinate={{latitude: 37.7765, longitude: -122.4424}} />
+          <Marker coordinate={{latitude: 37.7565, longitude: -122.4324}} />
+        </MapView>
+      </View>
     </SafeAreaView>
   );
 };
@@ -15,6 +33,10 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  map: {
+    width: '50%',
+    height: '50%',
   },
 });
 
